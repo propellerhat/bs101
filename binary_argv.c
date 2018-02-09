243909b5c94ba5455a65635aeee3655ddcdf5c6a
Jay Smith <jaysm17h@gmail.com>
2018-02-09 18:43:30 +0000

    Improve binary_argv.c

--- binary_argv.c
+++ binary_argv.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char *argv[]) {
   if (2 != argc) {
     printf("Usage: %s <key>\n", argv[0]);
     return 1;
   }
-  if (0 == strcmp(argv[1], "\xba\xdc\x0d\xe1")) {
-    printf("You win :P\n");
-    return 0;
+
+  if (0xbadc0de == *(int *)argv[1]) {
+    if (2147475906 == *(int *)(argv[1] + 5)) {
+      if (-2 == *(int *)(argv[1] + 13)) {
+        printf("You win :P\n");
+        return 0;
+      }
+    }
   }
+  
   printf("Sorry, '%s' is not the correct key :(\n", argv[1]);
+  
   return 1;
 }
